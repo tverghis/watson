8ffdd3a1cb3587b3e77f49ee08043cad0b30d9ff
Tarun Verghis <tarun.verghis@gmail.com>
2018-10-06 08:48:38 -0500

    Bot's first steps

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import Discord from 'discord.js';
+import Config from '../botconfig.json';
+
+const client = new Discord.Client();
+
+client.on("ready", () => {
+    console.log("Ready!");
+});
+
+client.login(Config.token);
